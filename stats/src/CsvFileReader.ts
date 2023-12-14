9ada78cf650b93da0b7a5bd706ad0b60e63b5176
suzannahthompson <suzannahthompson@hotmail.co.uk>
2023-12-14 16:48:48 +0000

    [Reusable code] - Add enums to index and add utils and CsvFileReader file

---
+++ stats/src/CsvFileReader.ts
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import {dateStringToDate} from "./utils";
+
+export class CsvFileReader{
+    data: string [] [] = [];
+
+
+    constructor(public filename:string) {
+    }
+
+    read():void{
+        this.data = fs
+            .readFileSync(this.filename, {
+                encoding: 'utf-8'
+            })
+            .split('\n')
+            .map((row:string):string [] => {
+                return row.split(',')
+            }).map ((row:string[]) => {
+                return [
+                    dateStringToDate(row[0]),
+                    row[1],
+                    row[2],
+                    parseInt(row[3]),
+                    parseInt(row[5]),
+                ]
+            })
+    }
+}
